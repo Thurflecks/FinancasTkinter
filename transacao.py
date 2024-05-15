8bf360f639560e35fd1a40e0722d9dd8735ddd43
Thurflecks <thurflecks.dev@gmail.com>
2024-05-15 15:11:52 -0300

    funcionando

--- transacao.py
+++ transacao.py
@@ -7,11 +7,7 @@ class Transacao:
     valor: float = 0
     descricao: str = 0
     categoria: Categoria = 0
-    
-    def adc_valor(self, v, d, c):
-        self.valor += v
-        self.descricao = d
-        self.categoria = c
+
     
     def getvalor(self):
         return self.valor
